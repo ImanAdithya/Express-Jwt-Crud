9c3b740bb8f8928448e096ec70aee07c3ffd2e57
ImanAdithya <adithyaiman07@gmail.com>
2024-02-08 08:35:36 +0530

    login auth succuss

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db/DBConnection');
+const jwt = require('jsonwebtoken');
+const dotenv=require('dotenv');
+
+
+dotenv.config();
+
+
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    db.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, result) => {
+        if (err) {
+            res.status(500).json({ error: 'Internal server error' });
+        } else if (result.length > 0) {
+            const user = { username: username };
+            const accessToken = jwt.sign(user, process.env.TOKEN_KEY,{expiresIn: '1h'});
+            res.json({ accessToken: accessToken });
+        } else {
+            res.status(401).json({ error: 'Invalid username or password' });
+        }
+    });
+});
+
 
 router.post('/register', (req, res) => {
     const { username, password } = req.body;
